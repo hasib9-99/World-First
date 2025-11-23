14d7eaa89eb3581a84dcaaf2e38b9edb0a847796
hasibforazitech-hue <hasib.forazitech@gmail.com>
2025-11-23 16:05:47 +0600

    video fream

--- src/components/home/VideoFrame.jsx
+++ src/components/home/VideoFrame.jsx
@@ -4,7 +4,7 @@ const VideoFrame = () => {
     return (
         <div id='video-frame' className='py-20 border-b border-b-[#555] mb-20'>
             <div className='max-w-[1200px] mx-auto h-[670px] relative'>
-                <div className='bg-[url("https://me3.io/video/bg.png")] w-[100%] h-full absolute bg-center bg-no-repeat bg-cover'></div>
+                <div className='bg-[url("https://me3.io/video/bg.png")] w-full h-full absolute bg-center bg-no-repeat bg-cover'></div>
                 <iframe className='w-full h-full left-0 top-0 absolute drop-shadow-[0_0_10px_rgba(255,54,215,.5019607843)]' src='https://player.vimeo.com/video/1011759662' allow="" title="Gacha Galaxy Sizzle Reel"></iframe>
                 <div></div>
             </div>
